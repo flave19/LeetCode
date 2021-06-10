08965724715a21d5a417d8826d4d47d5bd7e9f3a
flavian mohanraj <flavianmohanraj@gmail.com>
2021-06-10 08:35:44 -0400

    Maximum-subArray

---
+++ Easy/maximum-subArray.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maxSubArray = function(nums) {
+  let maxSum=-Infinity
+  let currentSum=-Infinity
+  
+  for(let i = 0; i< nums.length;i++){
+      currentSum = Math.max(nums[i],currentSum + nums[i])
+      maxSum = Math.max(currentSum, maxSum)
+  }
+  return maxSum
+}
